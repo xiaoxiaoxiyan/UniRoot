63c8033e7a97227f1d67200ef06f80ce85e767c1
xiaoxiaoxiyan <xiaoxiaoxiyan@users.noreply.github.com>
2026-06-13 22:46:41 +0000

    fix: 添加Kotlin Compose Compiler插件，修复Compose编译配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
 }
@@ -53,10 +54,6 @@ android {
 
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
 
     externalNativeBuild {
